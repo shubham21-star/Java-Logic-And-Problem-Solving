11b7f5bd643b30b4381b25c28486cb762b7153b5
SHUBHAM KUMAR <shubham.asp.da@gmail.com>
2026-09-23 10:11:37 +0530

    .\Check_Odd_or_Even.java

---
+++ 01-Coding-Blocks-DSA/Lecture-01/Check_Odd_or_Even.java
@@ -0,0 +1,10 @@
+class Check_Odd_or_Even {
+    public static void main(String[] args) {
+        int n = 10;
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
